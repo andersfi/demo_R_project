36e3980194868e9d6e496ec482d1d3fc8d08e593
andersfi <anders.finstad@ntnu.no>
2020-01-02 21:34:15 +0100

    Added download section of script

--- R/1_download_data.R
+++ R/1_download_data.R
@@ -12,6 +12,18 @@
 # first create new folder (./data/raw_data) to store the files 
 # locally (cache down the data) - remember to update gitignore folder (add 
 # the line */data/raw_data to the .gitignore file)
-
 dir.create("data/raw_data", showWarnings = FALSE, recursive = TRUE)
 
+# download data and save to ./data/raw_data (note the use of relative paths)
+URL <- "https://ndownloader.figshare.com/files/20551587"
+download.file(url=URL,destfile="./data/raw_data/temperatur.csv")  
+
+# open file
+temperature <- read.csv("./data/raw_data/temperatur.csv")
+
+
+
+
+
+  
+  
